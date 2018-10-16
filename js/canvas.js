1b21a5a9d6494f9162d77ee31c8060fe8288b6b1
未见山鬼 <1874163662@qq.com>
2018-10-16 17:49:41 +0800

    基本功能实现

--- js/canvas.js
+++ js/canvas.js
@@ -2,6 +2,8 @@ var canvas = document.getElementById('canvas');
 var oul = document.getElementById('pen');
 var oli = oul.getElementsByTagName('li');
 var context = canvas.getContext("2d");
+var slider = document.getElementsByClassName("slider")[0];
+
 var canvasPen = "#fffff";
 
 //设置画笔颜色
@@ -14,7 +16,6 @@ function handler(ele){
     ele.style.border = "2px solid #ddd";
     var penColor = colorRGB2Hex(getStyle(ele, 'background'));
     canvasPen = penColor;
-    console.log(canvasPen);
   }
 }
 for(var i=0; i<oli.length; i++){
@@ -25,21 +26,42 @@ for(var i=0; i<oli.length; i++){
 //鼠标按下时
 canvas.onmousedown = function(ev){
   var ev = ev || window.event;
-  disX = ev.clientX - canvas.offsetLeft;
-  disY = ev.clientY - canvas.offsetTop;
+  var start_x = ev.clientX - parseFloat(getStyle(slider, 'width')) - canvas.offsetLeft + document.body.scrollLeft;
+  var start_y = ev.clientY - canvas.offsetTop + document.body.scrollTop;
+  context.beginPath();
 
+  //移动画笔的初始位置
+  if(window.widthChange){
+    context.moveTo(start_x,start_y);
+  }else{
+    context.moveTo(start_x*(1-0.2),start_y*(1-0.2))
+  }
+  //设置画笔属性
+  context.lineCap = 'round';
+  context.lineJoin ="round";
+  context.lineWidth = 1;
+  context.strokeStyle = canvasPen;
   //鼠标移动时
-  alert(disX + "  " + disY);
-  document.onmousemove = function(ev){
-
+  canvas.onmousemove = function(ev){
+    var move_x = ev.clientX - parseFloat(getStyle(slider, 'width')) - canvas.offsetLeft + document.body.scrollLeft;
+    var move_y = ev.clientY - canvas.offsetTop + document.body.scrollTop;
+    //移动画笔的结束位置
+    if(window.widthChange){
+      context.lineTo(move_x,move_y);
+    }else{
+      context.lineTo(move_x*(1-0.2), move_y);
+    }
+    //开始绘制
+    context.stroke();
   };
 
   //鼠标抬起时
-  document.onmouseup = function(){
-    document.onmousemove = null;
-    document.onmouseup = null;
+  canvas.onmouseup = function(){
+    context.restore();
+    context.closePath();
+    canvas.onmousemove = null;
+    canvas.onmouseup = null;
   }
-  return false;
 }
 
 //获取元素的样式
@@ -56,7 +78,6 @@ function colorRGB2Hex(color) {
   var r = parseInt(rgb[0].split('(')[1]);
   var g = parseInt(rgb[1]);
   var b = parseInt(rgb[2].split(')')[0]);
-
   var hex = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   return hex;
 }
--- js/entry.js
+++ js/entry.js
@@ -3,20 +3,26 @@ window.onload = function(){
   var slider = document.getElementsByClassName("slider")[0];
   var content = document.getElementsByClassName("content")[0];
   var sliderContent = document.getElementsByClassName("sliderContent")[0];
+  var covans = document.getElementById("canvas");
 
   var flag = true; //slider打开与关闭的flag
   var timer = null; //slider滑动的定时器
   var sliderW = 20; //slider占款宽度的20%
   var contentW = 80;//content占宽度的80%
+  window.widthChange = true;
 
   slider.style.height = document.documentElement.clientHeight + 'px';
   content.style.height = document.documentElement.clientHeight + 'px';
+  //设置canvas的大小
+  canvas.width = parseFloat(getStyle(content, 'width'));
+  canvas.height = parseFloat(getStyle(content, 'height'))
   //按钮控制左侧滑动页
   menuBtn.onclick = function(){
     var speed = 1;    //滑动的速度
     var flagDisplay = true;
     clearInterval(timer);
     if(flag){
+      window.widthChange = false;
       flag = false;
       timer = setInterval(function(){
         if(sliderW > speed){ //这里需要注意一下，不能将sliderW设置为大于0，因为如果当sliderW-speed小于0是 走到else会出现闪屏现象
@@ -34,10 +40,14 @@ window.onload = function(){
         }else{
           slider.style.width = 0;
           content.style.width = "100%";
+          //调整canvas的大小
+        //  canvas.width = parseFloat(getStyle(content, 'width'));
+          //canvas.height = parseFloat(getStyle(content, 'height'));
           clearInterval(timer);
         }
       },20);
     }else{
+      window.widthChange = true;
       flag = true;
       timer = setInterval(function(){
         if(sliderW < (20-speed)){
@@ -52,9 +62,20 @@ window.onload = function(){
         }else{
           content.style.width = "80%";
           slider.style.width = "20%";
+          //调整canvas的大小
+          //canvas.width = parseFloat(getStyle(content, 'width'));
+          //canvas.height = parseFloat(getStyle(content, 'height'));
           clearInterval(timer);
         }
       },20);
     };
+  }
+  //获取元素的样式
+  function getStyle(el, name){
+    if(window.getComputedStyle){
+      return window.getComputedStyle(el)[name];
+    }else{
+      return el.currentStyle[name];
+    }
   }
 }
